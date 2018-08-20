a4d0ea580680d3e73737f3632c6d595ccc23d2a8
willnguyen <nctruong861990@gmail.com>
2018-08-20 07:15:57 +0000

    .

---
+++ 2nd_program_to_interface_not_implementation.rb
@@ -0,0 +1,13 @@
+# challence
+car = Car.new
+car.drive(100)
+
+plane = Plane.new
+plane.fly(1000)
+
+# solution
+vehicle1 = Vehicle.new
+vehicle1.traval(100)
+
+vehicle2 = Vehicle.new
+vehicle2.traval(100)
---
+++ 3rd_prefer_composition_over_inheritance.rb
@@ -0,0 +1,40 @@
+# Challenge
+class Vehicle
+	# All sorts of vehicle-related code...
+	def start_engine
+		# Start the engine
+	end
+	def stop_engine
+		# Stop the engine
+	end
+end
+
+class Car < Vehicle
+	def sunday_drive
+		start_engine
+			# Cruise out into the country and return
+		stop_engine
+	end
+end
+
+# Solution
+class Engine
+	# All sorts of engine-related code...
+	def start
+		# Start the engine
+	end
+	def stop
+		# Stop the engine
+	end
+end
+
+class Car
+	def initialize
+		@engine = Engine.new
+	end
+	def sunday_drive
+		@engine.start
+		# Cruise out into the country and return...
+		@engine.stop
+	end
+end
